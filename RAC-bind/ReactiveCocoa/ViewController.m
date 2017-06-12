759ba73cd54cabfde91366f84ac418ab02a3ce6b
changjian01 <768025549@qq.com>
2017-06-12 17:35:46 +0800

    fix grammar error
    fix grammar error

--- RAC-bind/ReactiveCocoa/ViewController.m
+++ RAC-bind/ReactiveCocoa/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "ReactiveCocoa.h"
-#import "RACReturnSignal.h"
+#import "ReactiveObjC.h"
+#import "ReactiveObjC/RACReturnSignal.h"
 
 //**RAC-bind**
 
@@ -25,9 +25,8 @@
     // 1.创建信号
     RACSubject *subject = [RACSubject subject];
     // 2.绑定信号
-   RACSignal *bindSignal = [subject bind:^RACStreamBindBlock{
-       // block调用时刻：只要绑定信号订阅就会调用。不做什么事情，
-        return ^RACSignal *(id value, BOOL *stop){
+    RACSignal *bindSignal = [subject bind:^RACSignalBindBlock _Nonnull{
+        return ^RACSignal *(id value, BOOL *stop) {
             // 一般在这个block中做事 ，发数据的时候会来到这个block。
             // 只要源信号（subject）发送数据，就会调用block
             // block作用：处理源信号内容
@@ -35,10 +34,10 @@
             value = @3; // 如果在这里把value的值改了，那么订阅绑定信号的值即44行的x就变了
             NSLog(@"接受到源信号的内容：%@", value);
             //返回信号，不能为nil,如果非要返回空---则empty或 alloc init。
-        return [RACReturnSignal return:value]; // 把返回的值包装成信号
+            return [RACReturnSignal return:value];
         };
     }];
-
+    
     // 3.订阅绑定信号
     [bindSignal subscribeNext:^(id x) {
         
@@ -57,7 +56,7 @@
                 /*************************总结**********************/
 
 // bind（绑定）的使用思想和Hook的一样---> 都是拦截API从而可以对数据进行操作，，而影响返回数据。
-// 发送信号的时候会来到30行的block。在这个block里我们可以对数据进行一些操作，那么35行打印的value和订阅绑定信号后的value就会变了。变成什么样随你喜欢喽。
+// 发送信号的时候会来到30行的block。在这个block里我们可以对数据进行一些操作，那么34行打印的value和订阅绑定信号后的value就会变了。变成什么样随你喜欢喽。
 
 
 
